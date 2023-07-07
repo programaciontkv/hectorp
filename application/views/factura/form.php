cd79d8569eafb937d47a77375f6cd006fef72d21
programaciontkv <programacion@tikvas.com>
2023-07-07 08:51:29 -0500

    Reduccion de iva en factura y nota de credito

--- application/views/factura/form.php
+++ application/views/factura/form.php
@@ -600,7 +600,7 @@ if ($this->session->flashdata('error')) {
                         <td valign="top" rowspan="9" colspan="<?php echo $col_obs ?>">
                           <textarea style="height: 80px; width: 90%;" id="observacion" name="observacion"   onkeydown="return enter(event)" maxlength="250" ><?php echo $factura->fac_observaciones ?></textarea>
                         </td>    
-                        <td colspan="2" align="right">Subtotal 12%:</td>
+                        <td colspan="2" align="right">Subtotal <?php echo $t_iva ?>%:</td>
                         <td>
                             <input style="text-align:right" type="text" class="form-control" id="subtotal12" name="subtotal12" value="<?php echo str_replace(',', '', number_format($factura->fac_subtotal12, $dec)) ?>" readonly/>
                             
@@ -896,6 +896,7 @@ var ctr_inv='<?php echo $ctrl_inv; ?>';
 var dec='<?php echo $dec; ?>';
 var dcc='<?php echo $dcc; ?>';
 var m_pag='<?php echo $m_pag; ?>';
+var t_iva = '<?php echo $t_iva ?>';
 var valida_asiento='<?php echo $valida_asiento; ?>';
 var objeto ;
 
@@ -1666,9 +1667,18 @@ function load_producto(j) {
             $('#pro_descripcion').val(dt.pro_codigo);
             $('#pro_referencia').val(dt.pro_descripcion);
             if(dt.pro_iva==''){
-              $('#iva').val('12');
+              ///$('#iva').val('12');
+              $('#iva').val(t_iva);
+
             }else{
+              //$('#iva').val(dt.pro_iva);
+
+              if (dt.pro_iva == '12') {
+              $('#iva').val(t_iva);
+              } else {
               $('#iva').val(dt.pro_iva);
+              }
+
             }
             $('#pro_aux').val(dt.pro_id);
             $('#pro_ids').val(dt.ids);
@@ -1923,15 +1933,20 @@ function calculo(obj) {
         tdsc = (round(tdsc,dec) * 1) + (round(d,dec) * 1);
         tice = (round(tice,dec) * 1) + (round(pic,dec) * 1);
 
-        if (ob == '14') {
-            t12 = (round(t12,dec) * 1 + round(vt,dec) * 1);
-            tiva = ((round(tice,dec) + round(t12,dec)) * 14 / 100);
-        }
+        // if (ob == '14') {
+        //     t12 = (round(t12,dec) * 1 + round(vt,dec) * 1);
+        //     tiva = ((round(tice,dec) + round(t12,dec)) * 14 / 100);
+        // }
 
 
-        if (ob == '12') {
-            t12 = (round(t12,dec) * 1 + round(vt,dec) * 1);
-            tiva = ((round(tice,dec) + round(t12,dec)) * 12 / 100);
+        // if (ob == '12') {
+        //     t12 = (round(t12,dec) * 1 + round(vt,dec) * 1);
+        //     tiva = ((round(tice,dec) + round(t12,dec)) * 12 / 100);
+        // }
+
+        if (ob != '0' && ob != 'EX' && ob != 'NO') {
+            t12 = (round(t12, dec) * 1 + round(vt, dec) * 1);
+            tiva = ((round(tice, dec) + round(t12, dec)) * t_iva / 100);
         }
 
         if (ob == '0') {
